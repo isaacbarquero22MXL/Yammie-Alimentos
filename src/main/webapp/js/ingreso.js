e0ee4b7e5fc420f0e24ae8a9c9cb71721cbbb770
IsaacMXL <isaacbarquero22@gmail.com>
2021-11-01 22:46:51 -0600

    Se agrego la página de "registro" para clientes con sus estilos css y metodos javascript.

--- src/main/webapp/js/ingreso.js
+++ src/main/webapp/js/ingreso.js
@@ -14,6 +14,7 @@ function animation() {
 }
 
 const input = document.querySelectorAll('.input');
+const combos = document.querySelectorAll('.login_location');
 function focusInput(){
     let parent = this.parentNode.parentNode;
     parent.classList.add('focus');
@@ -22,7 +23,7 @@ function focusInput(){
 function removeFocus(){
     let parent = this.parentNode.parentNode;
     if (this.value == "") {
-        parent.classList.remove('focus');
+        parent.classList.remove('focus'); 
     }
 }
 
@@ -31,13 +32,65 @@ input.forEach(input => {
     input.addEventListener('blur', removeFocus);
 });
 
+combos.forEach( combo => {
+    combo.addEventListener('click', focusInput);
+})
 
-// ==================Scroll Reveal Animation====================
 const sr = ScrollReveal({
     origin: 'top',
     distance: '60px',
     duration: 2000,
-    delay: 500
-});
+    delay: 300,
+})
 
-sr.reveal('.login_content');
+sr.reveal('.login_content'); 
+
+
+// =========== Sección de registro ===========
+
+let botones = document.querySelectorAll('.btn');
+let panelViewed = 1;
+let panels = document.querySelectorAll('.login_content-container');
+let divBtn = document.querySelectorAll('.login_btn');
+function moverPanel(){
+    if(panelViewed == 1){
+        panels[0].style.transform = 'translateX(-150%)';
+        panels[1].style.transform = 'translateX(-150%)';
+        divBtn[0].style.gridTemplateColumns = 'repeat(2, 1fr)';
+        showBtn();
+        panelViewed = 2;
+    }else{
+        panels[1].style.transform = 'translateX(150%)';
+        panels[0].style.transform = 'translateX(0)';
+        divBtn[0].style.gridTemplateColumns = 'repeat(1, 1fr)';
+        notShowBtn();
+        panelViewed = 1;
+    }
+}
+
+function onloadPage(){
+    notShowBtn();
+}
+
+
+function notShowBtn(){
+    botones.forEach(boton => {
+        boton.style.display = 'none';
+        botones[0].style.display = 'initial';
+    });
+}
+
+function showBtn(){
+    botones.forEach(boton => {
+        boton.style.display = 'initial';
+        botones[0].style.display = 'none';
+    });
+}
+
+window.onload = onloadPage();
+
+
+
+
+
+
